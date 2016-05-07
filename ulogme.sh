832037a1f40a949254b208dfd08bb56df2f9fdcb
Robin Schneider <ypid@riseup.net>
2016-05-08 00:23:53 +0200

    Use `xinput` to not depend on access to `/dev/console` (root only).
    Closes #34
    
    Additional improvements and fixes:
    
    * Why use `cat`, `grep` and `wc` combined when `grep` can do all of this
      at once? Fixed. [shellcheck](https://www.shellcheck.net/) can make you
      aware of such things.
    
    * Never storge highly sensitive information like keystrokes (which
      potentially can contain passwords) on persistent storage.
    
    * Terminate background processes when the main process gets killed.
    
    * Suppress `bash` warning when background process dies.
    
    * Updated and fixed README.md accordingly.
    
    * Removed trailing whitespace.

--- ulogme.sh
+++ ulogme.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+trap 'kill $(jobs -p)' EXIT
+
 if [ "$(uname)" == "Darwin" ]; then
   # This is a Mac
   ./osx/run_ulogme_osx.sh
 else
   # Assume Linux
-  sudo echo -n ""
-  sudo ./keyfreq.sh &
+  ./keyfreq.sh &
   ./logactivewin.sh
 fi
